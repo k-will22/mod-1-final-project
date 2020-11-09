ce30d7edd8e5840213749f0494aa3038edba03ed
Sara Cemal <you@example.com>
2020-11-09 17:56:15 -0500

    seeds part 2

--- db/migrate/seeds.rb
+++ db/migrate/seeds.rb
@@ -7,7 +7,14 @@ Song.destroy_all
 Playlist.destroy_all
 User.destroy_all
 
+Song.create [
+    {song_name: "POV", artist_name: "Ariana Grande", song_id: "", artist_id: "", song_length: "4 min", genre: "pop"}
+    {song_name: "vroom vroom ", artist_name: "charli xcx", song_id: "", artist_id: "", song_length: "2 min 54 sec", genre: "pop"}
+    {song_name: "broken flowers", artist_name: "danny l harle", song_id: "", artist_id: "", song_length: "", genre: ""}
+    {song_name: "one eye open", artist_name: "black marble", song_id: "", artist_id: "", song_length: "", genre: ""}
+    {song_name: "john wayne", artist_name: "lady gaga", song_id: "", artist_id: "", song_length: "", genre: ""}
 
+]
 
 # t.text :song_name
 # t.string :artist_name
