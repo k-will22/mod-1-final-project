a106b44feca9b5f9a76be2f758997ee0ff667961
k-will22 <thestreetkw@hotmail.com>
2020-11-09 17:44:22 -0500

    add playback

--- db/migrate/seeds.rb
+++ db/migrate/seeds.rb
@@ -3,3 +3,24 @@ require 'pry'
 require 'rest-client' # in order to make HTTP requests from a ruby file
 require 'json'
 
+Song.destroy_all 
+Playlist.destroy_all 
+
+api_resp = RestClient.get("https://api.spotify.com/v1")
+api_data = JSON.parse(api_resp)
+
+require "audio-playback"
+
+# Prompt the user to select an audio output
+@output = AudioPlayback::Device::Output.gets
+
+options = {
+  :channels => [0,1],
+  :latency => 1,
+  :output_device => @output
+}
+
+@playback = AudioPlayback.play("app/models/audio/Viva.m4a", options)
+
+# Play in the foreground
+@playback.block
